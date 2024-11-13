f4fcaa2318afde420e20f82068c808374fea8c89
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-13 21:45:05 +0200

    add the axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import axios from "axios";
 import ContentView from "./components/ContentView/ContentView";
 import { Header } from "./components/header/Header";
 import { MusicList } from "./components/MusicList/MusicList";
 import { MusicPlayer } from "./components/musicplayer/MusicPlayer";
 
 function App() {
+  axios({
+    method: "get",
+    url: "https://api.alquran.cloud/v1/quran/ar.alafasy",
+    responseType: "stream",
+  }).then((res) => {
+    const data = JSON.parse(res.data);
+    console.log(data.data.surahs[0].ayahs);
+  });
   return (
     <div className="App">
       <Header />
